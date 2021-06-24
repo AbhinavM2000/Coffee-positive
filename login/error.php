41168de24f5a6d07400beabc687a6607eceb8ef9
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2021-06-24 13:19:19 +0530

    Update error.php

--- login/error.php
+++ login/error.php
@@ -1,3 +1,17 @@
-<?php
-echo "Oopsie doopsie ! Invalid credentials ....";
-?>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=0.75">
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+<script>
+Swal.fire({
+  icon: 'error',
+  title: 'Oopsie doopsie...',
+  text: 'Invalid credentials, Try again.',
+}).then((result) => {
+  window.location.href = "index.html"
+})
+</script>
+</body>
+</html>
